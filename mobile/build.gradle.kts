b9381ae83373e99235b0f95d6f5f57eb30e7c47f
1Shin_ <49867372+1shin-7@users.noreply.github.com>
2026-03-16 23:47:28 +0800

    :bug: fix: use rootProject.file for keystore path resolution

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -33,7 +33,7 @@ extensions.configure<com.android.build.api.dsl.ApplicationExtension> {
     signingConfigs {
         if (useKeystore) {
             create("release") {
-                storeFile = file(keystoreProperties["storeFile"] as String)
+                storeFile = rootProject.file(keystoreProperties["storeFile"] as String)
                 storePassword = keystoreProperties["storePassword"] as String
                 keyAlias = keystoreProperties["keyAlias"] as String
                 keyPassword = keystoreProperties["keyPassword"] as String
